a6676eed5b9e12e5cedf4f74f12818025b0d1d32
Frank Su <Fsu0413@gmail.com>
2013-04-18 16:24:52 +0800

    Edit Wei004 Caochong AI

--- lua/ai/wei004-ai.lua
+++ lua/ai/wei004-ai.lua
@@ -1,13 +1,25 @@
 
 sgs.ai_skill_invoke.Wei004_Nieyi = function(self, data)
 	local damage = data:toDamage()
+	local caninvoke = true
 	if damage then
-		if (self:isWeak()) and (self.player:getHp() > 1) then
-			if not self:hasEquip() then 
-				return (damage.to:getHp() == 1) and (damage.to:getHandcardNum() == 0)
+		if self:isWeak() then
+			if self.player:getHp() == 1 then
+				caninvoke = false
+			else
+				if not ((damage.to:getHp() == 1) and (damage.to:getHandcardNum() == 0)) then
+					caninvoke = false
+				end
 			end
 		end
-		return self:isFriend(damage.to)
+		if self:hasEquip() then 
+			caninvoke = true
+		end
+		if caninvoke then
+			return self:isFriend(damage.to)
+		else
+			return false
+		end
 	end
 end
 
